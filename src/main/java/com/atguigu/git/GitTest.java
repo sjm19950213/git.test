ab4ef080c6ec7aa1f2b78f592f6e93aaf5495764
sjm <302768029@qq.com>
2021-06-04 16:54:55 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;/**
+ * 功能描述：
+ *
+ * @Author: sjm
+ * @Date: 2021/6/4 4:50 下午
+ */
+
+/**
+ *@Author shanjiaming
+ *@Since
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
